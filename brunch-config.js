3a3c6e2f645012c6fb160aee44a8e94b7995da14
David Balatero <dbalatero@gmail.com>
2017-10-04 21:52:46 -0700

    Working on it

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,18 @@
 // See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
-};
+module.exports = {
+  files:  {
+    javascripts: { joinTo: 'app.js' },
+    stylesheets: { joinTo: 'app.css' }
+  },
+  plugins: {
+    sass: {
+      mode: 'native',
+      options: {
+        includePaths: [
+          "app/styles",
+          "node_modules/normalize-scss/sass"
+        ]
+      }
+    }
+  }
+}
